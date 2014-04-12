1ef0591fc59fa1dcd2d2f371f9ba64cef6ab1f1f
Tiago de Pádua <tiagopadua@gmail.com>
2014-04-12 19:30:44 -0300

    nao sei

--- js/gallifreyan.js
+++ js/gallifreyan.js
@@ -13,7 +13,7 @@
     $.Graphic = function(targetCanvas) {
         this.name = "Graphic";
         this.canvas = typeof targetCanvas !== 'undefined' ? targetCanvas : null;
-        this.line_color = "#ffffff";
+        this.line_color = "#87b8e7";
         this.line_width = 2;
     }
     $.Graphic.prototype._draw = function(ctx) {
@@ -30,8 +30,12 @@
             return;
         }
         context = this.canvas.getContext("2d");
-        //context.shadowBlur = 10;
-        //context.shadowColor = "#ffff88";
+        context.shadowBlur = 5;
+        //context.shadowColor = "rgba(255,255,0,.4)";
+        //context.shadowColor = "rgba(60,180,220,.4)";
+        context.shadowColor = "rgba(0,0,0,.5)";
+        //context.shadowOffsetX = 4;
+        //context.shadowOffsetY = 4;
         context.beginPath();
         this._draw(context);
         context.stroke();
